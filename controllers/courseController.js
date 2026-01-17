2abeb9273a60e8210d27a5bc39b80638b69de410
nurrbb <enurb320@gmail.com>
2026-01-17 18:26:44 +0300

    feat: implement course controller for CRUD operations

---
+++ controllers/courseController.js
@@ -0,0 +1,18 @@
+const Course = require('../models/Course');
+
+exports.createCourse = async (req, res) => {
+  try {
+    const course = await Course.create({
+      name: req.body.name,
+      description: req.body.description,
+      category: req.body.category,
+      user: req.session.userID
+    });
+
+    req.flash("success", `${course.name} has been created successfully`);
+    res.status(201).redirect('/courses');
+  } catch (error) {
+    req.flash("error", `Something happened!`);
+    res.status(400).redirect('/courses');
+  }
+};
